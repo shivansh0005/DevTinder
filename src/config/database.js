007392afec011d4b952fa3d649e6e1a1e0f94603
Your Name <shivansh chaturvedi>
2025-03-22 22:51:50 +0530

    # Episode-06 | Database, Schema & Models | Mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@
 const mongoose=require('mongoose');
 
 const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://namasteshivansh:Hellomoto@namstenode1.cxr8b.mongodb.net/DevTinder');
+    await mongoose.connect('Take from gitignore');
 
 }
 module.exports=connectDB;
