1168a3a44a4169c870295999d7d9e90986334566
Shivansh Chaturvedi <112688356+shivansh0005@users.noreply.github.com>
2025-06-25 00:07:39 +0530

    Update sesClient.js

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { SESClient } =require("@aws-sdk/client-ses");
 
 // Set the AWS Region.
-const REGION = "eu-north-1"; // Correct region for Stockholm
-// Create SES service object.
+const REGION = "eu-north-1"; 
+
 const sesClient = new SESClient({ region: REGION ,credentials:{
     accessKeyId:process.env.AWS_ACCESS_KEY,
     secretAccessKey:process.env.AWS_SECRET_KEY
