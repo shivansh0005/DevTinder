26cb34e6a1cae3298bce5b285c586d37ed129a77
Shivansh Chaturvedi <112688356+shivansh0005@users.noreply.github.com>
2025-06-25 00:06:58 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,4 @@
-// const mongoose = require('mongoose');
 
-// const connectDB=async()=>{
-//     await mongoose.connect('mongodb+srv://namasteshivansh:Hellomoto@namstenode1.cxr8b.mongodb.net/DevTinder');
-// };
-// module.exports=connectDB;
 
 const mongoose=require('mongoose');
 
